e2ce031d91e02a1c91e8797382048515680e971b
Virgo-web <126802848+Virgo-web@users.noreply.github.com>
2025-04-08 17:58:17 -0500

    Delete setup.sql

--- setup.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE lounge_sloth;
-USE lounge_sloth;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    role ENUM('Buyer', 'Seller', 'Admin') NOT NULL
-);
