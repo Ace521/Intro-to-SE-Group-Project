9c8783464f93f519011ada92964e68ea060a33ff
Ace521 <78834879+Ace521@users.noreply.github.com>
2025-04-13 12:08:40 -0500

    Added products table to database

--- setup.sql
+++ setup.sql
@@ -8,6 +8,21 @@ CREATE TABLE users (
     role ENUM('Buyer', 'Seller', 'Admin') NOT NULL
 );
 
+CREATE TABLE products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    quantity INT DEFAULT 0,
+    
+    image_url VARCHAR(255),
+    seller_id INT,
+    
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (brand_id) REFERENCES brands(id),
+    FOREIGN KEY (seller_id) REFERENCES users(id)
+);
+
 ALTER TABLE users
 MODIFY role ENUM('Buyer', 'Seller', 'Admin') NULL;
 
